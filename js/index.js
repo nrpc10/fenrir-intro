3039fdd438a4e9de5fd9362214fe1ec4f11954ac
Nelson Palacios <nrpc10@gmail.com>
2023-07-19 09:48:30 -0500

    adding AJAX to my index.js

--- js/index.js
+++ js/index.js
@@ -83,3 +83,22 @@ messageForm.addEventListener('submit', function (e){
 
 });
 
+let githubRequest = new XMLHttpRequest();
+
+githubRequest.open("GET", "https://api.github.com/users/nrpc10/repos");
+githubRequest.send();
+
+githubRequest.addEventListener("load", (e) => {
+    let repositories = JSON.parse(githubRequest.responseText);
+    console.log(repositories);
+
+    let projectSection = document.getElementById('projects');
+    let projectList = projectSection.querySelector('ul');
+        for (let i=0; i<repositories.length; i++) {
+            let project = document.createElement('li');
+            project.innerHTML = repositories[i].name;
+            projectList.appendChild(project);
+            }
+    }   
+)
+
